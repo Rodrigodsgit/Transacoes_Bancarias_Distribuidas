b0e1d04895c1ae3f51b6cdb7d8e2c442eaf856f0
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2023-05-18 11:27:17 -0300

    Add: Testando relogios vetoriais

--- teste.py
+++ teste.py
@@ -1,15 +1,51 @@
-def determinar_ordem_vetorial(rel_vet_A, rel_vet_B, rel_vet_C):
-    for i in range(len(rel_vet_C)):
-        if rel_vet_A[i] < rel_vet_C[i]:
-            return "A ocorreu antes de B"
-        elif rel_vet_B[i] < rel_vet_C[i]:
-            return "B ocorreu antes de A"
-    return "A e B ocorreram simultaneamente ou têm a mesma ordem"
+class VectorClock:
+    def __init__(self):
+        self.clock = {}
 
-# Exemplo de relógios vetoriais
-rel_vet_A = [1, 0]
-rel_vet_B = [0, 1]
-rel_vet_C = [1, 1]
+    def increment(self, event):
+        if event not in self.clock:
+            self.clock[event] = 0
+        self.clock[event] += 1
 
-resultado = determinar_ordem_vetorial(rel_vet_A, rel_vet_B, rel_vet_C)
-print(resultado)
+    def compare(self, other):
+        for event in self.clock:
+            if event not in other.clock:
+                return 1
+            if self.clock[event] < other.clock[event]:
+                return -1
+            if self.clock[event] > other.clock[event]:
+                return 1
+        for event in other.clock:
+            if event not in self.clock:
+                return -1
+        return 0
+
+    def sync(self, other):
+        for event in other.clock:
+            if event not in self.clock or other.clock[event] > self.clock[event]:
+                self.clock[event] = other.clock[event]
+
+    def value(self):
+        return self.clock
+
+# Criando dois relógios vetoriais
+clock1 = VectorClock()
+clock2 = VectorClock()
+
+# Incrementando o relógio 1
+clock1.increment('A')
+
+# Incrementando o relógio 2
+clock2.increment('B')
+
+# Comparando os relógios
+result = clock1.compare(clock2)
+print(result)
+
+# Sincronizando os relógios
+clock1.sync(clock2)
+result = clock1.compare(clock2)
+print(result)
+
+print(clock1.value())
+print(clock2.value())
